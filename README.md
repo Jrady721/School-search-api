e45f083d108ae5680ddcf33d8d5f8dff1ece8903
Jrady <jrady721@gmail.com>
2018-12-31 15:13:44 +0900

    update

---
+++ README.md
@@ -0,0 +1,57 @@
+# School-search-api - 전국 학교 API
+
+### API 설명
+```
+이 API는 특정 검색어를 입력하면 검색어와 관련된 학교의 정보를 가져오는 API입니다.
+```
+
+### GET
+```
+http://jrady721.cafe24.com/api/school/검색어 (GET)
+```
+
+> **검색어 (search)**
+```
+형식: 문자열
+ex 소프트
+```
+
+> **예시 (example)**  
+
+http://jrady721.cafe24.com/api/school/소프트
+
+> Result:
+```json
+{
+    "status": "200",
+    "schools": [
+        {
+            "name": "대구소프트웨어고등학교",
+            "code": "D100000282",
+            "office": "dge.go.kr",
+            "level": 4,
+            "address": "대구광역시 달성군 구지면 창리로11길 93"
+        },
+        {
+            "name": "광주소프트웨어마이스터고등학교",
+            "code": "F100000120",
+            "office": "gen.go.kr",
+            "level": 4,
+            "address": "광주광역시 광산구 상무대로 312"
+        },
+        {
+            "name": "대덕소프트웨어마이스터고등학교",
+            "code": "G100000170",
+            "office": "dje.go.kr",
+            "level": 4,
+            "address": "대전광역시 유성구 가정북로 76"
+        }
+    ]
+}
+```
+
+> **활용**
+
+전국 급식 API: https://github.com/Jrady721/School-meal-api  
+웹 사이트: http://jrady721.cafe24.com/meal    
+구글(웨일) 확장프로그램: https://github.com/Jrady721/School-meal-extension
--- search.php
+++ search.php
@@ -1,32 +1,6 @@
 <?php
 include 'include/simple_html_dom.php';
 
-//$search = "대구소프트웨어";
-//
-//if($_GET) {
-//    $search = $_GET['search'];
-//}
-//
-//$search = iconv("UTF-8", "EUC-KR", $search);
-//$url = "https://www.schoolinfo.go.kr/ei/ss/Pneiss_f01_l0.do?SEARCH_SCHUL_NM=$search";
-//
-//// 20개 까지만 보여준다.
-//foreach (file_get_html($url)->find('.SchoolList') as $e) {
-//    // html 제거
-//    $e->find('span.InfoTitle', 1)->outertext = '';
-//    echo '지역: '.iconv("EUC-KR","UTF-8", $e->find('span.mapD_Area', 0)).'<br>';
-//    echo '구분: '.iconv("EUC-KR","UTF-8", $e->find('span.mapD_Class', 0)).'<br>';
-//    echo '주소: '.iconv("EUC-KR","UTF-8", $e->find('li', 1)->innertext).'<br>';
-//
-//
-//    $obj = $e->find('h1.School_Name a', 0);
-//    $schoolName = iconv("EUC-KR","UTF-8", $obj->innertext);
-//    preg_match("/\'.*\'/iU", $obj->onclick, $match);
-//    $match = str_replace("'","", $match[0]);
-//    echo "명칭: ". $schoolName.'<br>';
-//    echo "코드: ". $match.'<br><br>';
-//}
-
 // 학교정보 API, 이름, 소속, 구분, 주소, 코드 가져오기
 function getSchoolData($search = '') {
     Header('Content-Type: application/json');
@@ -43,14 +17,65 @@ function getSchoolData($search = '') {
 
     $json = array('status' => '200');
 
+    $schools = array();
 
-    // 20개 까지만
+    // 20개 까지만 (school info라는 사이트가 20개씩 보여준다..)
     foreach (file_get_html($url)->find('.SchoolList') as $e) {
         // html 제거
         $e->find('span.InfoTitle', 1)->outertext = '';
 
         $office = iconv("EUC-KR","UTF-8", $e->find('span.mapD_Area', 0)->innertext);
+
+        // 교육청별로 url 주소를 다리한다.
+        if($office == "서울") {
+            $office = "sen.go.kr";
+        } else if ($office == "경기") {
+            $office = "goe.go.kr";
+        } else if ($office == "강원") {
+            $office = "kwe.go.kr";
+        } else if ($office == "전남") {
+            $office = "jne.go.kr";
+        } else if ($office == "전북") {
+            $office = "jbe.go.kr";
+        } else if ($office == "경남") {
+            $office = "gne.go.kr";
+        } else if ($office == "경북") {
+            $office = "kbe.go.kr";
+        } else if ($office == "부산") {
+            $office = "pen.go.kr";
+        } else if ($office == "제주") {
+            $office = "jje.go.kr";
+        } else if ($office == "충남") {
+            $office = "cne.go.kr";
+        } else if ($office == "충북") {
+            $office = "cbe.go.kr";
+        } else if ($office == "광주") {
+            $office = "gen.go.kr";
+        } else if ($office == "울산") {
+            $office = "use.go.kr";
+        } else if ($office == "대전") {
+            $office = "dje.go.kr";
+        } else if ($office == "인천") {
+            $office = "ice.go.kr";
+        } else if ($office == "대구") {
+            $office = "dge.go.kr";
+        } else if ($office == "세종") {
+            $office = "sje.go.kr";
+        }
+
+        // level 즉 학교의 구분을 의미한다. (euc-kr 포맷을 utf-8 포맷으로 변경한다.)
         $level = iconv("EUC-KR","UTF-8", $e->find('span.mapD_Class', 0)->innertext);
+
+        if($level == "고") {
+            $level = 4;
+        } else if ($level == "중") {
+            $level = 3;
+        } else if ($level == "초") {
+            $level = 2;
+        } else if ($level == "특") { // 특수학교의 경우.. 그냥 level을 4로 맞춰었다. 정확히 테스트 해보지는 않았으나. level은 별로 상관없는 듯 하다.
+            $level = 4;
+        }
+
         $address = iconv("EUC-KR","UTF-8", $e->find('li', 1)->innertext);
 
 
@@ -60,13 +85,14 @@ function getSchoolData($search = '') {
         preg_match("/\'.*\'/iU", $obj->onclick, $match);
         $code = str_replace("'","", $match[0]);
 
-        $temp = array('code' => $code, 'office' => $office, 'level' => $level, 'address' => $address);
-        $json[$name] = $temp;
+        $temp = array('name'=> $name, 'code' => $code, 'office' => $office, 'level' => $level, 'address' => $address);
+        array_push($schools, $temp);
     }
 
-//    print_r($json);
+    $json['schools'] = $schools;
+
     return json_encode($json, JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT);
 }
 
-echo getSchoolData('소프트');
+ echo getSchoolData('소프트');
 ?>
